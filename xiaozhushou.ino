1f8f5ade82ad2bcecbdda10cf3155e9feb426564
Eussikun <57883305+loftea@users.noreply.github.com>
2022-05-29 19:42:05 +0800

    Update xiaozhushou.ino

--- xiaozhushou.ino
+++ xiaozhushou.ino
@@ -12,19 +12,23 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 
 /*
  * 提醒喝水小助手的源代码
- * 核心功能：如果红外检测处于高电平就会计时，计时到一定程度就会自动提醒。
+ * 核心功能：如果红外检测处于高电平就会计时，计时到一定时间就会自动提醒。
  * 拓展功能：
  * - 利用 DS3231 时间模块能够检测时间和温度，并且利用以上信息进行健康提醒
  * 变量：统计剩余时间数，已经喝水次数，以往喝水的加权平均，上次喝水的时间
- * 一些特性：只有在放回水杯之后才会刷新统计量，所以拿起水杯的时候不能马上看到屏幕更新
+ * 一些特性：
+ * - 只有在放回水杯之后才会刷新统计量，所以拿起水杯的时候不能马上看到屏幕更新
+ * - 使用的平均方式是指数加权平均，节约空间，并且更加科学
  */
 
-const int wuwuwu = 13; //提醒接口
-const int censor = 4;  //红外检测器
-// char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const int wuwuwu = 13;   //提醒接口
+const int censor = 4;    //红外检测器
+const int DEADLINE = 60; //设定的分钟数，建议设置 60 分钟
+const float lambda = 0.6; //加权权重，权重越大越在意最近的喝水间隔
 
-const int DEADLINE = 60;      //设定的分钟数，建议设置 60 分钟
-const float lambda = 0.6;     //加权权重，权重越大越在意最近的喝水间隔
+// char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday",
+// "Thursday", "Friday", "Saturday"};
+
 float averge_time = DEADLINE; //以往喝水间隔加权平均
 int last_time = DEADLINE;     //上次喝水的时间
 
@@ -34,41 +38,43 @@ float percent = 1.0;       //剩余倒计时百分比
 
 int length = 80; //矩阵长度
 
-void setup(void)
-{
+bool is_on = true; //表示程序正在运行，防止鸣叫时间过长的信息量
+
+void setup(void) {
   u8g2.begin();
   pinMode(wuwuwu, OUTPUT);
   pinMode(censor, INPUT);
   Serial.begin(9600);
 
   // 初始化rtc，
-  if (!rtc.begin())
-  {
+  if (!rtc.begin()) {
     Serial.println("Couldn't find RTC");
     Serial.flush();
     abort(); // 如果没有 RTC 停止运行
   }
 
-  if (rtc.lostPower())
-  {
+  if (rtc.lostPower()) {
     Serial.println("RTC lost power, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
 
-  // 一开始要显示初始化，不然一开始会黑屏
+  // 一开始要显示初始化，不然一开始一分钟会黑屏
   draw();
 }
 
-void loop(void)
-{
-  if (digitalRead(censor) == LOW)
-  {
-    while (digitalRead(censor) != HIGH) // 如果拿起水杯，则此循环等待水杯放回，放回后才会刷新统计量
+void loop(void) {
+  if (!is_on) {
+    while (digitalRead(censor) ==
+           HIGH) // 如果意外红外线被遮挡，会一直等待红外移开
+    {
+      delay(1000);
+    }
+    is_on = true;
+  }
+
+  if (digitalRead(censor) == LOW) {
+    while (digitalRead(censor) !=
+           HIGH) // 如果拿起水杯，则此循环等待水杯放回，放回后才会刷新统计量
     {
       delay(1000);
     }
@@ -80,13 +86,10 @@ void loop(void)
   {
     fresh_count = 60; //一分钟更新一次
 
-    if (count_down > 0)
-    {
+    if (count_down > 0) {
       last_time = DEADLINE - count_down; //上次喝水的时间，单位为分钟
       percent = static_cast<float>(count_down) / DEADLINE;
-    }
-    else if (!count_down)
-    {
+    } else if (!count_down) {
       ring();
     }
 
@@ -103,7 +106,7 @@ void loop(void)
 void draw() //更新 oled 屏幕显示
 {
   DateTime now = rtc.now();
-  
+
   u8g2.clearBuffer(); //清除缓存
   u8g2.setFont(u8g2_font_7x14B_tf);
 
@@ -115,10 +118,14 @@ void draw() //更新 oled 屏幕显示
   u8g2.print(now.day());
 
   u8g2.print(" ");
-  u8g2.print(rtc.getTemperature());
-  u8g2.print(" C");
+  u8g2.print(int(rtc.getTemperature()));
+  u8g2.print(" C"); // 显示温度
+
+  // 下面的功能显示星期，需要在全局变量中启用星期的数组
+  // u8g2.print(" ");
+  // u8g2.print(daysOfTheWeek[now.week()]);
+
   // u8g2.print("!!NotImplemented!!");
-  
 
   u8g2.setCursor(0, 13 + 13);
   u8g2.print("Average: ");
@@ -131,8 +138,9 @@ void draw() //更新 oled 屏幕显示
   u8g2.print(" mins ago");
 
   u8g2.setCursor(0, 50);
-  u8g2.print("water force:");
-  u8g2.drawRFrame(0, 52, length, 10, 2);
+  drawTips();
+
+  u8g2.drawRFrame(0, 52, length, 10, 2); // 绘制矩阵
   u8g2.drawBox(0, 52, length * percent, 10);
 
   u8g2.setCursor(length + 2, 62);
@@ -142,21 +150,57 @@ void draw() //更新 oled 屏幕显示
   u8g2.sendBuffer(); //显示
 }
 
-void freshArgs()
-{
+void drawTips() {
+  switch
+    rtc.now().hour() {
+    case 8:
+      u8g2.print("GOOD MORNING!");
+      break;
+    case 23:
+      u8g2.print("GOOD NIGHT!");
+      break;
+    default:
+      if (rtc.now().hour() < 9) {
+        u8g2.print("=_= zzZ");
+      } else {
+        u8g2.print("Water Matters!");
+      }
+  }
+}
+
+void freshArgs() {
   count_down = DEADLINE;
   fresh_count = 0;
   averge_time = averge_time * (1 - lambda) + last_time * lambda;
   last_time = 0;
 }
 
-void ring()
-{
-  while (digitalRead(censor) == HIGH) // 鸣叫
+void ring() {
+  int tmp = 0;                                    //防止鸣叫时间过长
+  while (digitalRead(censor) == HIGH && tmp < 30) // 鸣叫
   {
+    if (rtc.now().hour()<10 || rtc.now().hour()>=23 ){
+      continue;// 如果在睡觉时间，不会鸣叫
+    }
     digitalWrite(wuwuwu, HIGH);
     delay(300);
     digitalWrite(wuwuwu, LOW);
     delay(300);
+
+    tmp++;
   }
+
+  if (tmp >= 30) {
+    is_on = false;
+  }
+}
+
+void drawError() { //提示用户需要移开遮挡物
+  u8g2.clearBuffer(); //清除缓存
+  u8g2.setFont(u8g2_font_7x14B_tf);
+
+  u8g2.setCursor(0, 13);
+  u8g2.print("The laser is covered!");
+  u8g2.setCursor(0, 13 + 13);
+  u8g2.print("Please move it away!");
 }
